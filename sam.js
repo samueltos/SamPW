e30163c0e6bdbd60511eddb248fa4f7d0419812c
Samuel <samueltos@yahoo.com>
2019-11-01 18:31:47 -0400

    updates

--- sam.js
+++ sam.js
@@ -1,9 +1,3 @@
-const logo = document.querySelectorAll("#samsvg path");
-
-for (let i = 0; i < logo.length; i++){
-	console.log(`${i} : ${logo[i].getTotalLength()}`)
-}
-
 $('.collapse').on('hidden.bs.collapse', function () {
-    $('.collapse').eq(0).collapse('show');
+    $('.collapse').eq(1).collapse('show');
 })
