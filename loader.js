0e1988ac4aeeff8979de8b6334a1d67f3d9ec7f1
Samuel <samueltos@yahoo.com>
2019-11-04 21:27:53 -0500

    some

---
+++ loader.js
@@ -0,0 +1,16 @@
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 300);
+}
+
+function showPage() {
+  document.getElementById("mainLoader").style.display = "none";
+  document.getElementById("wholePage").style.display = "block";
+}
+
+
+// window.addEventListener("load", function(){
+// var load_screen = document.getElementById('load_screen');
+// document.body.removeChild(load_screen);
+// })
--- sam.js
+++ sam.js
@@ -1,3 +1,14 @@
 // $('.collapse').on('hidden.bs.collapse', function () {
 //     $('.collapse').eq(1).collapse('show');
 // })
+
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
