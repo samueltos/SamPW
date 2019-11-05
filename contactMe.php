d86b7cd3485f3cd1e6c750466a49aff7a24992fb
Samuel <samueltos@yahoo.com>
2019-11-05 15:35:32 -0500

    updatea

---
+++ contactMe.php
@@ -0,0 +1,17 @@
+<?php
+
+if($_POST["submit"]) {
+    $recipient="samueltos1@gmail.com";
+    $subject="New Message";
+    $sender=$_POST["name"];
+    $senderEmail=$_POST["email"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
+
+?>
