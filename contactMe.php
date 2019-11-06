873b483ca8c81df76fecbbb576e709dd5a524bb1
Samuel <samueltos@yahoo.com>
2019-11-06 13:15:15 -0500

    new commit

--- contactMe.php
+++ contactMe.php
@@ -1,5 +1,21 @@
 <?php
 
+// // Please specify your Mail Server - Example: mail.yourdomain.com.
+// ini_set("SMTP","mail.swordconsolutions.com");
+
+// // Please specify an SMTP Number 25 and 8889 are valid SMTP Ports.
+// ini_set("smtp_port","25");
+
+// // Please specify the return address to use
+// ini_set('sendmail_from', 'sam.akins@swordconsolutions.com');
+
+
+
+// ini_set('SMTP', "server.com");
+// ini_set('smtp_port', "25");
+// ini_set('sendmail_from', "email@domain.com");
+
+
 if($_POST["submit"]) {
     $recipient="samueltos1@gmail.com";
     $subject="New Message";
